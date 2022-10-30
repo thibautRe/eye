8b81a1e9bd3fc9f450dc78adb215bded0e9963c2
Thibaut REMY <thibaut@thibaut.re>
2022-10-30 16:02:41 -0400

    chore: correct weight implementation, add large size

--- packages-js/web/src/components/T/T.css.ts
+++ packages-js/web/src/components/T/T.css.ts
@@ -1,16 +1,13 @@
 import { style } from "@vanilla-extract/css"
 
-export const xs = style({
-  fontSize: "12px",
-  lineHeight: "12px",
-})
+export const sizes = {
+  xs: style({ fontSize: "12px", lineHeight: "12px" }),
+  s: style({ fontSize: "14px", lineHeight: "14px" }),
+  m: style({ fontSize: "16px", lineHeight: "16px" }),
+  l: style({ fontSize: "24px", lineHeight: "24px" }),
+}
 
-export const s = style({
-  fontSize: "14px",
-  lineHeight: "14px",
-})
-
-export const m = style({
-  fontSize: "16px",
-  lineHeight: "16px",
-})
+export const weights = {
+  normal: style({ fontWeight: "normal" }),
+  bold: style({ fontWeight: "bold" }),
+}
--- packages-js/web/src/components/T/T.tsx
+++ packages-js/web/src/components/T/T.tsx
@@ -4,14 +4,23 @@ import { Box, BoxProps } from "../Box/Box"
 import * as s from "./T.css"
 
 export interface TOwnProps {
-  t: "xs" | "s" | "m"
+  t: "xs" | "s" | "m" | "l"
   w?: "bold" | "normal"
 }
 export interface TProps extends TOwnProps, BoxProps {}
 
 export const T: Component<TProps> = p => {
-  const props = mergeProps({ w: "normal" }, p)
+  const props = mergeProps({ w: "normal" as const }, p)
   const [local, rest] = splitProps(props, ["t", "w", "classList"])
 
-  return <Box {...rest} classList={{ [s[local.t]]: true, ...classList }} />
+  return (
+    <Box
+      {...rest}
+      classList={{
+        [s.sizes[local.t]]: true,
+        [s.weights[local.w]]: true,
+        ...classList,
+      }}
+    />
+  )
 }
