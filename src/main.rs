79a50b20b2821e7872f8f51f5c8482ee0b1adb06
Thibaut REMY <thibaut@thibaut.re>
2022-11-23 21:52:52 -0500

    chore: move extract_pictures to its own file

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,17 @@ mod cli_args;
 mod database;
 mod errors;
 mod exif_helpers;
+mod extract_pictures;
 mod jwt;
 mod models;
 mod schema;
 
-use std::{
-  collections::{HashMap, HashSet},
-  path::Path,
-};
-
 use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 use api::api_routes;
 use cli_args::{Commands, Opt, ServeArgs};
-use database::{Pool, PooledConnection};
-use diesel::{QueryDsl, RunQueryDsl};
-use exif_helpers::*;
-use image::{imageops::FilterType, DynamicImage, GenericImageView, ImageError};
+use database::Pool;
+use extract_pictures::extract_pictures;
 use jwt::JwtKey;
-use models::{
-  camera_lenses::CameraLens,
-  picture::{Picture, PictureInsert},
-  picture_size::PictureSizeInsert,
-};
-use schema::pictures;
-use walkdir::WalkDir;
 
 type CommandReturn = Result<(), std::io::Error>;
 
@@ -56,114 +43,6 @@ async fn main() -> std::io::Result<()> {
     Commands::Serve(args) => start_server(args, pool).await,
     Commands::ExtractPictures => extract_pictures(opt.extract_from, opt.cache_path, pool),
   }
-}
-
-/// Filter pictures (JPG files)
-fn is_picture_file(file: &walkdir::DirEntry) -> bool {
-  file.file_type().is_file()
-    && file.file_name().to_str().map_or(false, |f| {
-      f.to_lowercase().ends_with(".jpg") || f.to_lowercase().ends_with(".jpeg")
-    })
-}
-
-fn extract_pictures(extract_from: String, cache_path: String, pool: Pool) -> CommandReturn {
-  let mut db = database::db_connection(&pool).unwrap();
-
-  let lenses = CameraLens::all().load::<CameraLens>(&mut db).unwrap();
-  let lenses_by_name: HashMap<String, CameraLens> = lenses
-    .into_iter()
-    .map(|lens| (lens.name.clone(), lens))
-    .collect();
-
-  let all_pictures: Vec<(String, i32)> = Picture::all()
-    .select((pictures::original_file_path, pictures::extract_version))
-    .load(&mut db)
-    .unwrap();
-  let all_pictures: HashSet<_> = all_pictures.into_iter().collect();
-
-  for entry in WalkDir::new(extract_from)
-    .follow_links(true)
-    .into_iter()
-    .filter_map(|e| e.ok())
-    .filter(|e| is_picture_file(e))
-  {
-    let file_path = entry.path().to_str().unwrap();
-    if all_pictures.contains(&(String::from(file_path), 1)) {
-      continue;
-    }
-    let entry_name = entry.file_name().to_str().unwrap();
-    let dyn_img = image::open(entry.path()).unwrap();
-    let exif = get_exif(&std::fs::File::open(entry.path())?);
-
-    let pic = PictureInsert {
-      name: Some(entry_name.into()),
-      uploaded_at: chrono::Local::now().naive_local(),
-      original_file_path: file_path.into(),
-      original_width: dyn_img.width() as i32,
-      original_height: dyn_img.height() as i32,
-      blurhash: create_blurhash(dyn_img.thumbnail(128, 128)).into(),
-      alt: "".into(),
-      access_type: None,
-      shot_by_user_id: None,        // TODO
-      shot_by_camera_body_id: None, // TODO
-
-      shot_by_camera_lens_id: get_shot_by_camera_lens_name(&exif)
-        .map(|name| lenses_by_name.get(name).map(|lens| lens.id))
-        .flatten(),
-      shot_at: get_shot_at(&exif),
-      shot_with_aperture: get_shot_with_fstop(&exif),
-      shot_with_focal_length: get_shot_with_focal_length(&exif),
-      shot_with_exposure_time: get_shot_with_exposure_time(&exif),
-      shot_with_iso: get_shot_with_iso(&exif),
-      extract_version: 1,
-    }
-    .insert(&mut db)
-    .unwrap();
-
-    let cache_path = Path::new(&cache_path);
-    for &(max_size, filter_type) in [
-      (2500, FilterType::CatmullRom),
-      (1800, FilterType::CatmullRom),
-      (1200, FilterType::CatmullRom),
-      (900, FilterType::Nearest),
-      (600, FilterType::Nearest),
-      (320, FilterType::Nearest),
-      (100, FilterType::Nearest),
-    ]
-    .iter()
-    {
-      create_subsize_picture(&pic, &dyn_img, max_size, filter_type, cache_path, &mut db).unwrap();
-    }
-  }
-  Ok(())
-}
-
-fn create_blurhash(img: DynamicImage) -> String {
-  let (width, height) = img.dimensions();
-  blurhash::encode(4, 3, width, height, &img.to_rgba8())
-}
-
-fn create_subsize_picture(
-  pic: &Picture,
-  img: &DynamicImage,
-  max_size: u32,
-  filter_type: FilterType,
-  cache_path: &Path,
-  db: &mut PooledConnection,
-) -> Result<(), ImageError> {
-  let pic_name = pic.name.clone().unwrap_or("unknown.jpg".into());
-  eprintln!("Resizing image {} for size {}...", pic_name, max_size);
-  let resized_img = img.resize(max_size, max_size, filter_type);
-  let file_path = max_size.to_string() + "-" + &rand::random::<u64>().to_string() + "-" + &pic_name;
-  PictureSizeInsert {
-    picture_id: pic.id,
-    height: resized_img.height() as i32,
-    width: resized_img.width() as i32,
-    file_path: file_path.clone(),
-  }
-  .insert(db)
-  .unwrap();
-  resized_img.save(cache_path.join(file_path))
 }
 
 async fn start_server(args: ServeArgs, pool: Pool) -> CommandReturn {
