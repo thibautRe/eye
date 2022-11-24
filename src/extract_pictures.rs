fc947a20d421699fd97505ace9a33ecdc3161680
Thibaut REMY <thibaut@thibaut.re>
2022-11-23 22:18:06 -0500

    chore: extract single picture extraction

--- src/extract_pictures.rs
+++ src/extract_pictures.rs
@@ -5,7 +5,7 @@ use std::{
 
 use diesel::{QueryDsl, RunQueryDsl};
 use image::{imageops::FilterType, DynamicImage, GenericImageView, ImageError};
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 
 use crate::{
   database::{self, Pool, PooledConnection},
@@ -34,6 +34,7 @@ pub fn extract_pictures(extract_from: String, cache_path: String, pool: Pool) ->
     .unwrap();
   let all_pictures: HashSet<_> = all_pictures.into_iter().collect();
 
+  let cache_path = Path::new(&cache_path);
   for entry in WalkDir::new(extract_from)
     .follow_links(true)
     .into_iter()
@@ -44,49 +45,59 @@ pub fn extract_pictures(extract_from: String, cache_path: String, pool: Pool) ->
     if all_pictures.contains(&(String::from(file_path), 1)) {
       continue;
     }
-    let entry_name = entry.file_name().to_str().unwrap();
-    let dyn_img = image::open(entry.path()).unwrap();
-    let exif = get_exif(&std::fs::File::open(entry.path())?);
+    process_picture(entry, &lenses_by_name, cache_path, &mut db)?;
+  }
+  Ok(())
+}
 
-    let pic = PictureInsert {
-      name: Some(entry_name.into()),
-      uploaded_at: chrono::Local::now().naive_local(),
-      original_file_path: file_path.into(),
-      original_width: dyn_img.width() as i32,
-      original_height: dyn_img.height() as i32,
-      blurhash: create_blurhash(dyn_img.thumbnail(128, 128)).into(),
-      alt: "".into(),
-      access_type: None,
-      shot_by_user_id: None,        // TODO
-      shot_by_camera_body_id: None, // TODO
+fn process_picture(
+  entry: DirEntry,
+  lenses_by_name: &HashMap<String, CameraLens>,
+  cache_path: &Path,
+  db: &mut PooledConnection,
+) -> Result<(), std::io::Error> {
+  let file_path = entry.path().to_str().unwrap();
+  let entry_name = entry.file_name().to_str().unwrap();
+  let dyn_img = image::open(entry.path()).unwrap();
+  let exif = get_exif(&std::fs::File::open(entry.path())?);
 
-      shot_by_camera_lens_id: get_shot_by_camera_lens_name(&exif)
-        .map(|name| lenses_by_name.get(name).map(|lens| lens.id))
-        .flatten(),
-      shot_at: get_shot_at(&exif),
-      shot_with_aperture: get_shot_with_fstop(&exif),
-      shot_with_focal_length: get_shot_with_focal_length(&exif),
-      shot_with_exposure_time: get_shot_with_exposure_time(&exif),
-      shot_with_iso: get_shot_with_iso(&exif),
-      extract_version: 1,
-    }
-    .insert(&mut db)
-    .unwrap();
+  let pic = PictureInsert {
+    name: Some(entry_name.into()),
+    uploaded_at: chrono::Local::now().naive_local(),
+    original_file_path: file_path.into(),
+    original_width: dyn_img.width() as i32,
+    original_height: dyn_img.height() as i32,
+    blurhash: create_blurhash(dyn_img.thumbnail(128, 128)).into(),
+    alt: "".into(),
+    access_type: None,
+    shot_by_user_id: None,        // TODO
+    shot_by_camera_body_id: None, // TODO
 
-    let cache_path = Path::new(&cache_path);
-    for &(max_size, filter_type) in [
-      (2500, FilterType::CatmullRom),
-      (1800, FilterType::CatmullRom),
-      (1200, FilterType::CatmullRom),
-      (900, FilterType::Nearest),
-      (600, FilterType::Nearest),
-      (320, FilterType::Nearest),
-      (100, FilterType::Nearest),
-    ]
-    .iter()
-    {
-      create_subsize_picture(&pic, &dyn_img, max_size, filter_type, cache_path, &mut db).unwrap();
-    }
+    shot_by_camera_lens_id: get_shot_by_camera_lens_name(&exif)
+      .map(|name| lenses_by_name.get(name).map(|lens| lens.id))
+      .flatten(),
+    shot_at: get_shot_at(&exif),
+    shot_with_aperture: get_shot_with_fstop(&exif),
+    shot_with_focal_length: get_shot_with_focal_length(&exif),
+    shot_with_exposure_time: get_shot_with_exposure_time(&exif),
+    shot_with_iso: get_shot_with_iso(&exif),
+    extract_version: 1,
+  }
+  .insert(db)
+  .unwrap();
+
+  for &(max_size, filter_type) in [
+    (2500, FilterType::CatmullRom),
+    (1800, FilterType::CatmullRom),
+    (1200, FilterType::CatmullRom),
+    (900, FilterType::Nearest),
+    (600, FilterType::Nearest),
+    (320, FilterType::Nearest),
+    (100, FilterType::Nearest),
+  ]
+  .iter()
+  {
+    create_subsize_picture(&pic, &dyn_img, max_size, filter_type, cache_path, db).unwrap();
   }
   Ok(())
 }
