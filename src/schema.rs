812ff6a24ee4546fdbe7bd1d05e7a59f3449fdc2
Thibaut Remy <thibaut@thibaut.re>
2023-09-26 21:42:56 +0200

    chore: update diesel

--- src/schema.rs
+++ src/schema.rs
@@ -1,4 +1,6 @@
-table! {
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
     albums (id) {
         id -> Int4,
         name -> Text,
@@ -8,21 +10,21 @@ table! {
     }
 }
 
-table! {
+diesel::table! {
     camera_bodies (id) {
         id -> Int4,
         name -> Varchar,
     }
 }
 
-table! {
+diesel::table! {
     camera_lenses (id) {
         id -> Int4,
         name -> Varchar,
     }
 }
 
-table! {
+diesel::table! {
     picture_albums (id) {
         id -> Int8,
         picture_id -> Int4,
@@ -30,7 +32,7 @@ table! {
     }
 }
 
-table! {
+diesel::table! {
     picture_sizes (id) {
         id -> Int4,
         picture_id -> Int4,
@@ -40,7 +42,7 @@ table! {
     }
 }
 
-table! {
+diesel::table! {
     picture_tags (id) {
         id -> Int4,
         picture_id -> Int4,
@@ -48,7 +50,7 @@ table! {
     }
 }
 
-table! {
+diesel::table! {
     picture_user_access (id) {
         id -> Int4,
         user_id -> Int4,
@@ -56,7 +58,7 @@ table! {
     }
 }
 
-table! {
+diesel::table! {
     pictures (id) {
         id -> Int4,
         name -> Nullable<Text>,
@@ -79,7 +81,7 @@ table! {
     }
 }
 
-table! {
+diesel::table! {
     tags (id) {
         id -> Int4,
         name -> Text,
@@ -87,9 +89,10 @@ table! {
     }
 }
 
-table! {
+diesel::table! {
     users (id) {
         id -> Int4,
+        #[max_length = 120]
         email -> Varchar,
         name -> Varchar,
         created_at -> Timestamp,
@@ -97,18 +100,18 @@ table! {
     }
 }
 
-joinable!(picture_albums -> albums (album_id));
-joinable!(picture_albums -> pictures (picture_id));
-joinable!(picture_sizes -> pictures (picture_id));
-joinable!(picture_tags -> pictures (picture_id));
-joinable!(picture_tags -> tags (tag_id));
-joinable!(picture_user_access -> pictures (picture_id));
-joinable!(picture_user_access -> users (user_id));
-joinable!(pictures -> camera_bodies (shot_by_camera_body_id));
-joinable!(pictures -> camera_lenses (shot_by_camera_lens_id));
-joinable!(pictures -> users (shot_by_user_id));
+diesel::joinable!(picture_albums -> albums (album_id));
+diesel::joinable!(picture_albums -> pictures (picture_id));
+diesel::joinable!(picture_sizes -> pictures (picture_id));
+diesel::joinable!(picture_tags -> pictures (picture_id));
+diesel::joinable!(picture_tags -> tags (tag_id));
+diesel::joinable!(picture_user_access -> pictures (picture_id));
+diesel::joinable!(picture_user_access -> users (user_id));
+diesel::joinable!(pictures -> camera_bodies (shot_by_camera_body_id));
+diesel::joinable!(pictures -> camera_lenses (shot_by_camera_lens_id));
+diesel::joinable!(pictures -> users (shot_by_user_id));
 
-allow_tables_to_appear_in_same_query!(
+diesel::allow_tables_to_appear_in_same_query!(
     albums,
     camera_bodies,
     camera_lenses,
