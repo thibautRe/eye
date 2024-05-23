0d74f5ec761c4c573e97061a2669fc511bd33092
Thibaut Remy <thibaut@thibaut.re>
2024-05-23 22:33:51 +0200

    chore: delete unused files

--- packages-js/web/src/components/Routes/newFile.tsx
+++
@@ -1,6 +0,0 @@
-import { useParams } from "@solidjs/router"
-
-export default () => {
-  const params = useParams<{ slug: string }>()
-  const [postRes, postResAction] = createResource(params.slug, apiGetPost)
-}
